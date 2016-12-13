0dd8d496ab50cff854f91b6339fe5c06e0d44d4d
surunzi <surunzi@foxmail.com>
2016-12-13 14:37:37 +0800

    Add: invertTree

---
+++ invertTree.js
@@ -0,0 +1,13 @@
+module.exports = function invertTree(root)
+{
+    if (!root) return root;
+
+    var tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
